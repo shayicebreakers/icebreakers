6b13534ef3da320ff42d59478a0f47275bd3a8d1
Rohan Bhangui <bhanguirohan@gmail.com>
2018-11-08 20:25:19 -0500

    updated stuff

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-var array = [
-]
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var questionsCachedArr = JSON.parse(localStorage.getItem('questions'));
+var randomQuestion = '';
+
+// if cached
+if(questionsCachedArr) {
+	randomQuestion = questionsCachedArr[getRandomInt(questionsCachedArr.length)][0];
+}
+//if not cached then retrieve
+else {
+	var request = new XMLHttpRequest();  
+	request.open("GET", 'data.csv', false);   
+	request.send(null);  
+
+	var csvData = new Array();
+	var jsonObject = request.responseText.split(/\r?\n|\r/);
+	for (var i = 0; i < jsonObject.length; i++) {
+	  csvData.push(jsonObject[i].split(','));
+	}
+
+	localStorage.setItem('questions', JSON.stringify(csvData));
+	questionsCachedArr = JSON.parse(localStorage.getItem('questions'));
+
+	randomQuestion = questionsCachedArr[getRandomInt(questionsCachedArr.length)][0];
+}
+
+
+document.querySelector(".question").innerHTML = randomQuestion;
+
